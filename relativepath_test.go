89daf5811b3140df0f6d7adfe665c96e15e68a25
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-07-17 17:36:15 +0200

    more tests

--- relativepath_test.go
+++ relativepath_test.go
@@ -9,13 +9,29 @@ type testPath struct {
 }
 
 func TestCreateRelativePath(t *testing.T) {
-	test := []testPath{
+	t.Log("Testing CreatePathRoot")
+	testCreateRootPath := []testPath{
+		{"///a///b  c/d", "", "/a/b  c/d"},
+		{"", "", "/"},
+		{"/!\"§$%&/()=}", "", "/!\"§$%&/()=}"},
+		{"a/b/", "", "/a/b"}}
+	for _, set := range testCreateRootPath {
+		path := CreatePathRoot(set.root)
+		result := path.FullPath()
+		if result != set.want {
+			t.Error("Expected", set.want, "got", result)
+		}
+	}
+	t.Log("Testing CreatePath")
+	testCreatePath := []testPath{
 		{"/a", "b", "/a/b"},
 		{"/a/b", "c/d", "/a/b/c/d"},
 		{"/a", "/b/", "/a/b"},
 		{"a", "b", "/a/b"},
-		{"/////a/////", "///b///c/d//", "/a/b/c/d"}}
-	for _, set := range test {
+		{"/////a/////", "///b///c/d//", "/a/b/c/d"},
+		{"", "a/b", "/a/b"},
+		{"a///b/c/d", "", "/a/b/c/d"}}
+	for _, set := range testCreatePath {
 		path := CreatePath(set.root, set.sub)
 		result := path.FullPath()
 		if result != set.want {
