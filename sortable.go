daeef094f685dc36c4e0a3fda30427d241d63bb0
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-07-26 17:36:20 +0200

    added sortable []string

--- sortable.go
+++ sortable.go
@@ -17,3 +17,21 @@ func (s Sortable) Less(i, j int) bool {
 	// path are sorted alphabetically all by themselves! :D
 	return s[i].Path < s[j].Path
 }
+
+/*
+SortableString is a string slice that can be sorted by length.
+*/
+type SortableString []*string
+
+func (s SortableString) Len() int {
+	return len(s)
+}
+
+func (s SortableString) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortableString) Less(i, j int) bool {
+	// path are sorted alphabetically all by themselves! :D
+	return *s[i] < *s[j]
+}
