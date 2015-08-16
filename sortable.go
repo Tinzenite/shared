5a515307d997a61e93bb4910488db4a0d691ab0e
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-08-16 16:58:22 +0200

    added another sortable type

--- sortable.go
+++ sortable.go
@@ -1,5 +1,7 @@
 package shared
 
+//TODO all these sort things all sort string paths basically. Can this be improved?
+
 /*
 Sortable allows sorting Objectinfos by path.
 */
@@ -16,6 +18,23 @@ func (s Sortable) Swap(i, j int) {
 func (s Sortable) Less(i, j int) bool {
 	// path are sorted alphabetically all by themselves! :D
 	return s[i].Path < s[j].Path
+}
+
+/*
+SortableUpdateMessage allows the sorting of UpdateMessages
+*/
+type SortableUpdateMessage []*UpdateMessage
+
+func (s SortableUpdateMessage) Len() int {
+	return len(s)
+}
+
+func (s SortableUpdateMessage) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortableUpdateMessage) Less(i, j int) bool {
+	return s[i].Object.Path < s[j].Object.Path
 }
 
 /*
