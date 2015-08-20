7a919d3334c629d4326708cc6d51646f8381e800
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-08-20 16:59:41 +0200

    better Version_Equal test

--- version_test.go
+++ version_test.go
@@ -2,14 +2,42 @@ package shared
 
 import "testing"
 
+type testEqual struct {
+	one  Version
+	two  Version
+	want bool
+}
+
 func TestVersion_Equal(t *testing.T) {
-	verA := Version{}
-	verB := Version{}
-	if !verA.Equal(verB) {
-		t.Error("A should equal B")
-	}
-	if !verB.Equal(verA) {
-		t.Error("B should equal A")
+	testEquals := []testEqual{
+		// empty
+		{Version{}, Version{}, true},
+		{Version{}, Version{"a": 0}, false},
+		{Version{}, Version{"a": 0, "b": 12}, false},
+		// single
+		{Version{"a": 0}, Version{"a": 0}, true},
+		{Version{"a": 0}, Version{"a": 1}, false},
+		{Version{"a": 0}, Version{"b": 0}, false},
+		{Version{"a": 0}, Version{"b": 1}, false},
+		// multiple
+		{Version{"a": 0, "b": 0}, Version{"a": 0, "b": 0}, true},
+		{Version{"a": 0, "b": 0}, Version{"b": 0, "a": 0}, true},
+		{Version{"a": 0, "b": 0}, Version{"a": 1, "b": 0}, false},
+		{Version{"a": 0, "b": 0}, Version{"a": 0, "b": 2}, false},
+		{Version{"a": 0, "b": 0}, Version{"a": 1, "b": 1}, false},
+		// mixed
+		{Version{"a": 0}, Version{"b": 0, "a": 0}, false},
+		{Version{"a": 12}, Version{"b": 1, "a": 34}, false},
+		{Version{"a": 12}, Version{"b": 1, "c": 34}, false}}
+	for _, test := range testEquals {
+		oneEqualTwo := test.one.Equal(test.two)
+		twoEqualOne := test.two.Equal(test.one)
+		if oneEqualTwo != twoEqualOne {
+			t.Error("Equal not symmetrical!", test.one, test.two)
+		}
+		if oneEqualTwo != test.want {
+			t.Error("Expected", test.want, "got", oneEqualTwo, "for", test.one, test.two)
+		}
 	}
 }
 
